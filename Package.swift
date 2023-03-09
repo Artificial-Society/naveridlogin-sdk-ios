36cce5d2ea9fa0cb26a619f56663b549ce26f8fb
asc-ChoiChef <117614971+ChoiChef@users.noreply.github.com>
2023-03-09 15:33:50 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,6 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
+// sdk-version:4.1.5
 let package = Package(
     name: "naveridlogin-sdk-ios",
     products: [
